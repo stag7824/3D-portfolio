53016c138bb3f186babc92044e8b2f1449149e0a
angrybudha2 <syed.gardezi@stud.uni-corvinus.hu>
2024-10-28 17:58:42 +0500

    chore: just pushing for home laptop

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Canvas} from '@react-three/fiber';
+{/* THE POPUP */}
+{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+  POPUP
+</div> */}
+const Home = () => {
+  return (
+    <section className='w-full h-screen relative'>
+      <Canvas className='bg-transparent w-full h-screen'>
+        
+      </Canvas>
+    </section>
+  )
+}
+
+export default Home;
