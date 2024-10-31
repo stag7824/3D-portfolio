dbaa101d0b982f18ab4cfb143fde930022e8669d
stag7824 <atalha54@gmail.com>
2024-10-31 14:01:08 +0500

    feat: almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { Home, About, Projects, Contact } from './pages'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-export default function App() {
+import { Footer, Navbar } from "./components";
+import { About, Contact, Home, Projects } from "./pages";
+
+const App = () => {
   return (
     <main className='bg-slate-300/20'>
       <Router>
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/contact' element={<Contact />} />
+          <Route
+            path='/*'
+            element={
+              <>
+                <Routes>
+                  <Route path='/about' element={<About />} />
+                  <Route path='/projects' element={<Projects />} />
+                  <Route path='/contact' element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </main>
-  )
-}
+  );
+};
+
+export default App;
---
+++ src/models/index.js
@@ -0,0 +1,9 @@
+
+import Bird from './Bird'
+import Sky from './Sky'
+import Plane from './Plane'
+import Island from './Island'
+import Fox from './Fox'
+
+
+export { Bird, Sky, Plane, Island, Fox };
